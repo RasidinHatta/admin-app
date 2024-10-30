e393ae02b3a22e8048f9efb3370478d3d016d903
RasidinHatta <129116066+RasidinHatta@users.noreply.github.com>
2024-10-30 11:13:47 +0800

    sadas
    asdas

---
+++ src/components/spline-text.tsx
@@ -0,0 +1,11 @@
+import Spline from '@splinetool/react-spline/next';
+
+export default function Object() {
+  return (
+    <main>
+      <Spline
+        scene="https://prod.spline.design/JlwYCMyCUVmTc1oo/scene.splinecode" 
+      />
+    </main>
+  );
+}
